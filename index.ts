0c48b82cfbfb265a998f2cbdc51c10d8495af958
mehreenprogrammer <152857879+mehreenprogrammer@users.noreply.github.com>
2024-01-25 23:49:00 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+;import chalk from "chalk";
+import inquirer from "inquirer"
+
+const apilink = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple";
+
+let fetchData = async(data:string) =>{
+let fetchQuiz:any = await fetch(data)
+let res = await fetchQuiz.json()
+return res.results;  
+}
+
+let data = await fetchData(apilink);
+
+let startQuiz = async () => {
+    let score:number = 0
+    // for user name
+    let name = await inquirer.prompt({
+        type:"input",
+        name:"hello",
+        message:"what is your name?"
+    });
+
+    for(let i = 1 ; i<=5 ; i++){
+        let answers = [...data[i].incorrect_answers,data[i].
+        correct_answer];
+
+        let ans = await inquirer.prompt({
+            type:"list",
+            name:"quiz",
+            message:data[i].question,
+            choices: answers.map((val:any)=> val),
+        });
+       
+        if(ans.quiz == data[i].correct_answer){
+            ++score;
+            console.log(chalk.bold.italic.blue("Correct"))
+        } else{
+            console.log(`correct answer is ${chalk.bold. 
+                italic.red(data[i].correct_answer)}`)
+        }
+    }
+    
+    console.log(`dear ${chalk.green.bold(name.hello)},your score is ${chalk.red.bold(score)}out of ${chalk.red.bold('5')}`);
+};
+
+startQuiz();
